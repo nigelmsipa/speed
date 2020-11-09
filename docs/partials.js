0476c35a267ff1323b2c32b8c590a446a47f85cc
nigelmsipa <nigelmsipa@gmail.com>
2020-11-09 08:12:33 -0500

    fixing top nav

--- docs/partials.js
+++ docs/partials.js
@@ -7,13 +7,13 @@ fetch('./docs/sidebar.html')
   .then((data) => {
     document.querySelector('.sidebar-content').innerHTML = data;
   });
-fetch('./docs/topnav.html')
-  .then((response) => {
-    return response.text();
-  })
-  .then((data) => {
-    document.querySelector('.top-nav-content').innerHTML = data;
-  });
+// fetch('./docs/topnav.html')
+//   .then((response) => {
+//     return response.text();
+//   })
+//   .then((data) => {
+//     document.querySelector('.top-nav-content').innerHTML = data;
+//   });
 fetch('./docs/footer.html')
   .then((response) => {
     return response.text();
