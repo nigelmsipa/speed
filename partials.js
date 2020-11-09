974e73a0dcaa8849aafa92aed65caebe8f78ed14
nigelmsipa <nigelmsipa@gmail.com>
2020-11-09 01:46:39 -0500

    css

--- partials.js
+++ partials.js
@@ -21,32 +21,3 @@ fetch('./docs/footer.html')
   .then((data) => {
     document.querySelector('.footer-content').innerHTML = data;
   });
-
-$(function () {
-  const simpleBarEnabled =
-    document.getElementsByClassName('js-simplebar').length > 0;
-  const simpleBarInstance = simpleBarEnabled
-    ? new SimpleBar(document.getElementsByClassName('js-simplebar')[0])
-    : null;
-
-  /* Sidebar toggle behaviour */
-  $('.sidebar-toggle').on('click', function () {
-    $('.sidebar')
-      .toggleClass('toggled')
-      // Triger resize after animation
-      .one('transitionend', function () {
-        setTimeout(function () {
-          window.dispatchEvent(new Event('resize'));
-        }, 100);
-      });
-  });
-
-  const active = $('.sidebar .active');
-
-  if (active.length && active.parent('.collapse').length) {
-    const parent = active.parent('.collapse');
-
-    parent.prev('a').attr('aria-expanded', true);
-    parent.addClass('show');
-  }
-});
